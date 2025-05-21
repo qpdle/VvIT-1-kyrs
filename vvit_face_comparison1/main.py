b06ceeb5fcdbb2ed316b4f713a55bcf4741244e8
Орлов Даниил <145991779+qpdle@users.noreply.github.com>
2025-05-21 15:34:22 +0300

    Add files via upload

---
+++ vvit_face_comparison1/main.py
@@ -0,0 +1,14 @@
+import face_recognition
+
+known_image = face_recognition.load_image_file("known.jpg")
+unknown_image = face_recognition.load_image_file("unknown.jpg")
+
+known_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+
+if results[0]==False:
+    print("Люди на фото не похожи")
+if results[0]==True:
+    print("Л.ди на фото похожи")
